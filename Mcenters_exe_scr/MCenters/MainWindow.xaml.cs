21c1b0e641b82bd9d5b4a7975e04a1f7fe08d532
misike12 <59843249+misike12@users.noreply.github.com>
2022-11-13 11:47:54 +0100

    Add files via upload

---
+++ Mcenters_exe_scr/MCenters/MainWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using mcenters;
+
+namespace M_Centers
+{
+	// Token: 0x02000003 RID: 3
+	public partial class MainWindow : Window
+	{
+		// Token: 0x06000004 RID: 4 RVA: 0x0000208C File Offset: 0x0000028C
+		public MainWindow()
+		{
+			this.InitializeComponent();
+			API.EnableDebug();
+			Logger.SetLogControl(this.outputBox);
+			string[] commandLineArgs = Environment.GetCommandLineArgs();
+			bool flag = commandLineArgs != null;
+			if (flag)
+			{
+				string text = null;
+				bool flag2 = commandLineArgs.Length > 1;
+				if (flag2)
+				{
+					text = commandLineArgs[1];
+				}
+				bool flag3 = !string.IsNullOrEmpty(text);
+				if (flag3)
+				{
+					bool flag4 = text == "trial-enable";
+					if (flag4)
+					{
+						this.trial = true;
+						this.trialBox.IsChecked = new bool?(true);
+						this.PerformHack();
+					}
+					bool flag5 = text == "trial-disable";
+					if (flag5)
+					{
+						this.trial = false;
+						this.trialBox.IsChecked = new bool?(false);
+						this.PerformHack();
+					}
+				}
+			}
+		}
+
+		// Token: 0x06000005 RID: 5 RVA: 0x0000215B File Offset: 0x0000035B
+		private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			Environment.Exit(0);
+		}
+
+		// Token: 0x06000006 RID: 6 RVA: 0x00002168 File Offset: 0x00000368
+		private void TextBlock_MouseEnter(object sender, MouseEventArgs e)
+		{
+			TextBlock textBlock = sender as TextBlock;
+			textBlock.Background = null;
+			textBlock.Foreground = new SolidColorBrush(Colors.Red);
+			textBlock.FontWeight = FontWeights.ExtraBold;
+		}
+
+		// Token: 0x06000007 RID: 7 RVA: 0x000021A4 File Offset: 0x000003A4
+		private void TextBlock_MouseLeave(object sender, MouseEventArgs e)
+		{
+			TextBlock textBlock = sender as TextBlock;
+			textBlock.Foreground = new SolidColorBrush(Colors.Black);
+			textBlock.Background = new SolidColorBrush(Colors.Red);
+			textBlock.FontWeight = FontWeights.Bold;
+		}
+
+		// Token: 0x06000008 RID: 8 RVA: 0x000021E8 File Offset: 0x000003E8
+		private void PerformHack()
+		{
+			Logger.Clear();
+			bool flag = this.trial;
+			if (flag)
+			{
+				Logger.AddLog("Selected Minecraft Mode = Enable Trial");
+			}
+			else
+			{
+				Logger.AddLog("Selected Minecraft Mode = Enable Full Game");
+			}
+			API.PrepareProcess(this.trial);
+		}
+
+		// Token: 0x06000009 RID: 9 RVA: 0x0000222A File Offset: 0x0000042A
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			this.PerformHack();
+		}
+
+		// Token: 0x0600000A RID: 10 RVA: 0x00002234 File Offset: 0x00000434
+		private void CheckBox_Checked(object sender, RoutedEventArgs e)
+		{
+			this.trial = true;
+		}
+
+		// Token: 0x0600000B RID: 11 RVA: 0x0000223E File Offset: 0x0000043E
+		private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+		{
+			this.trial = false;
+		}
+
+		// Token: 0x0600000C RID: 12 RVA: 0x00002248 File Offset: 0x00000448
+		private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			bool flag = e.LeftButton == MouseButtonState.Pressed;
+			if (flag)
+			{
+				base.DragMove();
+			}
+		}
+
+		// Token: 0x04000001 RID: 1
+		private bool trial = false;
+	}
+}
---
+++ Mcenters_exe_scr/MCenters/Properties/Resources.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+namespace M_Centers.Properties
+{
+	// Token: 0x02000004 RID: 4
+	[GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+	[DebuggerNonUserCode]
+	[CompilerGenerated]
+	internal class Resources
+	{
+		// Token: 0x0600000F RID: 15 RVA: 0x000023AE File Offset: 0x000005AE
+		internal Resources()
+		{
+		}
+
+		// Token: 0x17000001 RID: 1
+		// (get) Token: 0x06000010 RID: 16 RVA: 0x000023B8 File Offset: 0x000005B8
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		internal static ResourceManager ResourceManager
+		{
+			get
+			{
+				bool flag = Resources.resourceMan == null;
+				if (flag)
+				{
+					ResourceManager resourceManager = new ResourceManager("M_Centers.Properties.Resources", typeof(Resources).Assembly);
+					Resources.resourceMan = resourceManager;
+				}
+				return Resources.resourceMan;
+			}
+		}
+
+		// Token: 0x17000002 RID: 2
+		// (get) Token: 0x06000011 RID: 17 RVA: 0x00002400 File Offset: 0x00000600
+		// (set) Token: 0x06000012 RID: 18 RVA: 0x00002417 File Offset: 0x00000617
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		internal static CultureInfo Culture
+		{
+			get
+			{
+				return Resources.resourceCulture;
+			}
+			set
+			{
+				Resources.resourceCulture = value;
+			}
+		}
+
+		// Token: 0x04000005 RID: 5
+		private static ResourceManager resourceMan;
+
+		// Token: 0x04000006 RID: 6
+		private static CultureInfo resourceCulture;
+	}
+}
